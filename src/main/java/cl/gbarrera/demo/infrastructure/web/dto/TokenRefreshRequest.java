a749ef5cbb7fca0fccc6f661a2f99fa593b4fb2a
Gonza <barrerasaezgonzalo@gmail.com>
2025-07-06 22:24:09 -0400

    remove redis y small refactor

--- src/main/java/cl/gbarrera/demo/infrastructure/web/dto/TokenRefreshRequest.java
+++ src/main/java/cl/gbarrera/demo/infrastructure/web/dto/TokenRefreshRequest.java
@@ -1,5 +1,6 @@
 package cl.gbarrera.demo.infrastructure.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenRefreshRequest {
+    @JsonProperty("refresh_token")
     private String refreshToken;
 }
